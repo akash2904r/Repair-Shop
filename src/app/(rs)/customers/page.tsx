0e6f046159163f6044f7c155f43109378224e870
Akash <akash2904r@gmail.com>
2025-03-18 21:04:40 +0530

    Add search funtionality for customers and tickets page

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,7 +1,26 @@
+import { getCustomerSearchResults } from "@/lib/queries/getCustomerSearchResults";
+
+import CustomerSearch from "@/app/(rs)/customers/CustomerSearch";
+
 export const metadata = {
     title: "Customers",
 }
 
-export default function Customers() {
-    return <h2>Customers Page</h2>
+export default async function Customers({
+    searchParams,
+}: {
+    searchParams: Promise<{ [key: string]: string | undefined }>
+}) {
+    const { searchText } = await searchParams;
+
+    if (!searchText) return <CustomerSearch />
+
+    const results = await getCustomerSearchResults(searchText);
+
+    return (
+        <>
+            <CustomerSearch />
+            <p>{JSON.stringify(results)}</p>
+        </>
+    );
 }
